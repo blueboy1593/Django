f6bb9387e1b1bc999a851a560751f96752346535
Kang Hyun <snb0303@naver.com>
2019-08-22 12:29:23 +0900

    Register Article model to admin page
     - Customize admin page

--- articles/admin.py
+++ articles/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Article
+# 동일한 모델에 있는 아티클을 등록. admin이라는 미리 주어진 객체를 활용
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'content', 'created_at', 'updated_at',)
 
 # Register your models here.
+admin.site.register(Article)
