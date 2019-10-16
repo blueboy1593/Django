72133d598c9de16c73af5f50c6fb3932cecc4324
blueboy1593 <snb0303@naver.com>
2019-10-16 17:52:57 +0900

    homeworkshop done

--- articles/models.py
+++ articles/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import EmailValidator, MinValueValidator
 
 
 class Article(models.Model):
@@ -9,3 +10,19 @@ class Article(models.Model):
 
     class Meta:
         ordering = ('-pk', )
+
+
+class Person(models.Model):
+    name = models.CharField(max_length=10)
+    email = models.CharField(
+        max_length=100,
+        validators=[EmailValidator(message='이메일 형식에 맞지 않습니다.')]
+        )
+    age = models.IntegerField(
+        validators=[MinValueValidator(19, message="미성년자는 노노에요")]
+    )
+
+class Musician(models.Model):
+    # PK = models.IntegerField()
+    FIRST_NAME = models.CharField(max_length=20)
+    LAST_NAME = models.CharField(max_length=20)
